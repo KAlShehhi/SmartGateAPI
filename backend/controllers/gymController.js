6cbaee5044dcd4913285a989c3d9fe8c5736989d
KAlShehhi <Khalifa.asaas@gmail.com>
2023-12-07 05:14:11 +0400

    GYM CRUD functinalities

--- backend/controllers/gymController.js
+++ backend/controllers/gymController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require('express-async-handler');
+const Gym = require('../models/gymModel');
 // @desc    Get gyms
 // @route   GET /api/gyms
 // @access  Private
 const getGyms = asyncHandler(async (req, res) => {
-    res.status(200).json({message : 'Get Gym'})
+    const gyms = await Gym.find();
+    res.status(200).json(gyms);
 });
 
 // @desc    Create gym
@@ -14,20 +16,37 @@ const createGym = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('Please add a name field');
     }
-    res.status(200).json({message : 'Create Gym'})
+    const gym = await Gym.create({
+        name: req.body.name
+    });
+    res.status(200).json(gym)
 });
 
 // @desc    Update gym
 // @route   PUT /api/gyms/:id
 // @access  Private
 const updateGym = asyncHandler (async (req, res) => {
-    res.status(200).json({message : `Update Gym ${req.params.id}`})
+    const gym = await Gym.findById(req.params.id);
+    if(!gym){
+        res.status(400);
+        throw new Error('Gym not found!');
+    }
+    const updatedGym = await Gym.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+    res.status(200).json({message : `Update Gym ${req.params.id}`, updatedGym});
 });
 
 // @desc    Delete gym
 // @route   DELETE /api/gyms/:id
 // @access  Private
 const deleteGym = asyncHandler(async (req, res) => {
+    const gym = await Gym.findById(req.params.id);
+    if(!gym){
+        res.status(400);
+        throw new Error('Gym not found!');
+    }
+    const deletedGym = await Gym.findByIdAndDelete(req.params.id);
     res.status(200).json({message : `Delete Gym ${req.params.id}`})
 });
 
