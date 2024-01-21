2e5ca287d2bf9f6b13f931ee38bc2aee0d3d7e9d
Khalifa AlShehhi <Khalifa.asaas@gmail.com>
2024-01-21 08:43:18 +0400

    Added gate controller and routes files

---
+++ backend/routes/gateRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { } = require('../controllers/gateController');
+
+const {protect} = require('../middleware/authMiddleware');
+
+
+module.exports = router;
